f430a172d8ff2bc3baece686faf2333e874de7a0
HeyBlack <zhfleo@163.com>
2025-04-11 16:37:07 +0800

    config structure

--- src/main/java/heyblack/mineds/config/ConfigOption.java
+++ src/main/java/heyblack/mineds/config/ConfigOption.java
@@ -5,11 +5,13 @@ public enum ConfigOption {
     URL("url", "https://api.siliconflow.cn/v1/chat/completions"),
     MODEL("model", "deepseek-ai/DeepSeek-R1"),
     API_KEY("api_key", "YOUR_API_KEY"),
-    MAX_REQUEST("max_request", "1"),
     TEMPERATURE("temperature", "0.7"),
     MAX_TOKENS("max_tokens", "4096"),
     SYSTEM_MESSAGE("system_message", "你是一只傲娇猫娘"),
-    AI_NAME("ai_name", "DeepSeek")
+
+    // In-game behaviour settings
+    MAX_REQUEST("max_request", "1"),
+    AI_NAME("ai_name", "DeepSeek"),
     ;
 
     public final String id;
--- src/main/java/heyblack/mineds/config/ConfigScreen.java
+++ src/main/java/heyblack/mineds/config/ConfigScreen.java
@@ -13,50 +13,55 @@ public class ConfigScreen {
         ConfigBuilder builder = ConfigBuilder.create()
                 .setParentScreen(parent)
                 .setTitle(new TranslatableText("mineds.config.title"));
-
-        ConfigCategory api = builder.getOrCreateCategory(new TranslatableText("mineds.config.category.api"));
-
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
-        api.addEntry(entryBuilder.startStrField(new TranslatableText("mineds.config.option.url"), CONFIG_MANAGER.get(ConfigOption.URL.id))
-                .setDefaultValue(ConfigOption.URL.defaultValue)
-                .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.URL.id, newValue))
-                .build());
+        {
+            ConfigCategory api = builder.getOrCreateCategory(new TranslatableText("mineds.config.category.api"));
 
-        api.addEntry(entryBuilder.startStrField(new TranslatableText("mineds.config.option.api_key"), CONFIG_MANAGER.get(ConfigOption.API_KEY.id))
-                .setDefaultValue(ConfigOption.API_KEY.defaultValue)
-                .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.API_KEY.id, newValue))
-                .build());
+            api.addEntry(entryBuilder.startStrField(new TranslatableText("mineds.config.option.url"), CONFIG_MANAGER.get(ConfigOption.URL.id))
+                    .setDefaultValue(ConfigOption.URL.defaultValue)
+                    .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.URL.id, newValue))
+                    .build());
 
-        api.addEntry(entryBuilder.startStrField(new TranslatableText("mineds.config.option.model"), CONFIG_MANAGER.get(ConfigOption.MODEL.id))
-                .setDefaultValue(ConfigOption.MODEL.defaultValue)
-                .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.MODEL.id, newValue))
-                .build());
+            api.addEntry(entryBuilder.startStrField(new TranslatableText("mineds.config.option.api_key"), CONFIG_MANAGER.get(ConfigOption.API_KEY.id))
+                    .setDefaultValue(ConfigOption.API_KEY.defaultValue)
+                    .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.API_KEY.id, newValue))
+                    .build());
 
-        api.addEntry(entryBuilder.startStrField(new TranslatableText("mineds.config.option.sys_message"), CONFIG_MANAGER.get(ConfigOption.SYSTEM_MESSAGE.id))
-                .setDefaultValue(ConfigOption.SYSTEM_MESSAGE.defaultValue)
-                .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.SYSTEM_MESSAGE.id, newValue))
-                .build());
+            api.addEntry(entryBuilder.startStrField(new TranslatableText("mineds.config.option.model"), CONFIG_MANAGER.get(ConfigOption.MODEL.id))
+                    .setDefaultValue(ConfigOption.MODEL.defaultValue)
+                    .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.MODEL.id, newValue))
+                    .build());
 
-        api.addEntry(entryBuilder.startStrField(new TranslatableText("mineds.config.option.ai_name"), CONFIG_MANAGER.get(ConfigOption.AI_NAME.id))
-                .setDefaultValue(ConfigOption.AI_NAME.defaultValue)
-                .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.AI_NAME.id, newValue))
-                .build());
+            api.addEntry(entryBuilder.startStrField(new TranslatableText("mineds.config.option.sys_message"), CONFIG_MANAGER.get(ConfigOption.SYSTEM_MESSAGE.id))
+                    .setDefaultValue(ConfigOption.SYSTEM_MESSAGE.defaultValue)
+                    .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.SYSTEM_MESSAGE.id, newValue))
+                    .build());
 
-        api.addEntry(entryBuilder.startIntField(new TranslatableText("mineds.config.option.max_request"), Integer.parseInt(CONFIG_MANAGER.get(ConfigOption.MAX_REQUEST.id)))
-                .setDefaultValue(Integer.parseInt(ConfigOption.MAX_REQUEST.defaultValue))
-                .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.MAX_REQUEST.id, String.valueOf(newValue)))
-                .build());
+            api.addEntry(entryBuilder.startIntField(new TranslatableText("mineds.config.option.max_tokens"), Integer.parseInt(CONFIG_MANAGER.get(ConfigOption.MAX_TOKENS.id)))
+                    .setDefaultValue(Integer.parseInt(ConfigOption.MAX_TOKENS.defaultValue))
+                    .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.MAX_TOKENS.id, String.valueOf(newValue)))
+                    .build());
 
-        api.addEntry(entryBuilder.startIntField(new TranslatableText("mineds.config.option.max_tokens"), Integer.parseInt(CONFIG_MANAGER.get(ConfigOption.MAX_TOKENS.id)))
-                .setDefaultValue(Integer.parseInt(ConfigOption.MAX_TOKENS.defaultValue))
-                .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.MAX_TOKENS.id, String.valueOf(newValue)))
-                .build());
+            api.addEntry(entryBuilder.startDoubleField(new TranslatableText("mineds.config.option.temperature"), Double.parseDouble(CONFIG_MANAGER.get(ConfigOption.TEMPERATURE.id)))
+                    .setDefaultValue(Double.parseDouble(CONFIG_MANAGER.get(ConfigOption.TEMPERATURE.id)))
+                    .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.TEMPERATURE.id, String.valueOf(newValue)))
+                    .build());
+        }
 
-        api.addEntry(entryBuilder.startDoubleField(new TranslatableText("mineds.config.option.temperature"), Double.parseDouble(CONFIG_MANAGER.get(ConfigOption.TEMPERATURE.id)))
-                .setDefaultValue(Double.parseDouble(CONFIG_MANAGER.get(ConfigOption.TEMPERATURE.id)))
-                .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.TEMPERATURE.id, String.valueOf(newValue)))
-                .build());
+        {
+            ConfigCategory inGameBehaviour = builder.getOrCreateCategory(new TranslatableText("mineds.config.category.in_game_behaviour"));
+
+            inGameBehaviour.addEntry(entryBuilder.startStrField(new TranslatableText("mineds.config.option.ai_name"), CONFIG_MANAGER.get(ConfigOption.AI_NAME.id))
+                    .setDefaultValue(ConfigOption.AI_NAME.defaultValue)
+                    .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.AI_NAME.id, newValue))
+                    .build());
+
+            inGameBehaviour.addEntry(entryBuilder.startIntField(new TranslatableText("mineds.config.option.max_request"), Integer.parseInt(CONFIG_MANAGER.get(ConfigOption.MAX_REQUEST.id)))
+                    .setDefaultValue(Integer.parseInt(ConfigOption.MAX_REQUEST.defaultValue))
+                    .setSaveConsumer(newValue -> CONFIG_MANAGER.setConfig(ConfigOption.MAX_REQUEST.id, String.valueOf(newValue)))
+                    .build());
+        }
 
         return builder.build();
     }
