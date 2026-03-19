c480d74885046e58de53f1d6b2ae7c2a29d4481c
HeyBlack <zhfleo@163.com>
2026-03-19 19:22:21 +0800

    add url in result log

--- src/main/java/heyblack/mineds/util/result/ApiCallResult.java
+++ src/main/java/heyblack/mineds/util/result/ApiCallResult.java
@@ -1,6 +1,8 @@
 package heyblack.mineds.util.result;
 
 import com.google.gson.JsonObject;
+import heyblack.mineds.config.ConfigManager;
+import heyblack.mineds.config.ConfigOption;
 
 import java.time.Instant;
 
@@ -9,12 +11,14 @@ public class ApiCallResult {
     public JsonObject output;
     public Status status;
     public String timestamp;
+    public String url;
 
     public ApiCallResult(JsonObject input, JsonObject output, boolean isSuccess) {
         this.input = input;
         this.output = output;
         this.status = isSuccess ? Status.SUCCESS : Status.FAIL;
         this.timestamp = Instant.now().toString();
+        this.url = ConfigManager.getInstance().get(ConfigOption.URL.id);
     }
 
     enum Status {
